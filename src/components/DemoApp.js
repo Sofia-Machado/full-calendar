ff69d746933d7f55ac4a7a1c432b952ff575e32d
Sofia Machado <sofia.machado@riva-office.pt>
2023-04-17 16:56:25 +0100

    css

--- src/components/DemoApp.js
+++ src/components/DemoApp.js
@@ -302,91 +302,89 @@ export function DemoApp() {
 
   return (
     <div className='calendar-app'>
-      <Container>
         <h1>Call-endar</h1>
-        <Stack spacing={2} sx={{ width: 300 }}>
-        <Autocomplete
-          id="search-by-category"
-          options={categoryOptions}
-          groupBy={(option) => option.extendedProps.category}
-          getOptionLabel={(option) => option.title}
-          sx={{ width: 300 }}
-          onChange={(e, value) => {
-            console.log(value)
-            setEventInfo(value);
-            handleOpenCreateForm()
-          }}
-          renderInput={(params) => <TextField {...params}
-          InputProps={{
-            ...params.InputProps,
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            )
-          }}
-          label="Search by category" />}
-        />
-        </Stack>
-        
+      <Container>
+        <div className='mui-forms'>
+          <Stack spacing={2} sx={{ ml: 25, width: 300 }}>
+          <Autocomplete
+            id="search-by-category"
+            options={categoryOptions}
+            groupBy={(option) => option.extendedProps.category}
+            getOptionLabel={(option) => option.title}
+            sx={{ width: 300 }}
+            onChange={(e, value) => {
+              console.log(value)
+              setEventInfo(value);
+              handleOpenCreateForm()
+            }}
+            renderInput={(params) => <TextField {...params}
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+            label="Search by category" />}
+          />
+          </Stack>
+          <FormControl sx={{ mt: 2, width: 300 }}>
+            <InputLabel id="demo-multiple-chip-label">Filter</InputLabel>
+            <Select
+              labelId="demo-multiple-chip-label"
+              id="demo-multiple-chip"
+              multiple
+              value={filters}
+              onChange={handleFilter}
+              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              //MenuProps={MenuProps}
+            >            
+              {filtersList.map((filter) => (
+                <MenuItem
+                  key={filter}
+                  value={filter}
+                  style={getStyles(filter, filtersList, theme)}
+                >
+                  {filter}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
         <FormControl>
-          <FormLabel id="slot-duration-select">Slot Duration</FormLabel>
-          <RadioGroup
-            aria-labelledby="slot-duration-select-options"
-            value={slotDuration}
-            onChange={(e) => setSlotDuration(e.target.value)}
-            name="slot-duration-select-options"
-            sx={{ display: "block" }}
-          >
-            <FormControlLabel
-              value="00:15:00"
-              control={<Radio size="small" />}
-              label="15min"
-            />
-            <FormControlLabel
-              value="00:30:00"
-              control={<Radio size="small" />}
-              label="30min"
-            />
-            <FormControlLabel
-              value="01:00:00"
-              control={<Radio size="small" />}
-              label="1h"
-            />
-          </RadioGroup>
-        </FormControl>
-
-        <FormControl sx={{ m: 1, width: 120 }}>
-          <InputLabel id="demo-multiple-chip-label">Filter</InputLabel>
-          <Select
-            labelId="demo-multiple-chip-label"
-            id="demo-multiple-chip"
-            multiple
-            value={filters}
-            onChange={handleFilter}
-            input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-            renderValue={(selected) => (
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                {selected.map((value) => (
-                  <Chip key={value} label={value} />
-                ))}
-              </Box>
-            )}
-            //MenuProps={MenuProps}
-          >
-
-          
-            {filtersList.map((filter) => (
-              <MenuItem
-                key={filter}
-                value={filter}
-                style={getStyles(filter, filtersList, theme)}
-              >
-                {filter}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+            <FormLabel id="slot-duration-select">Slot Duration</FormLabel>
+            <RadioGroup
+              aria-labelledby="slot-duration-select-options"
+              value={slotDuration}
+              onChange={(e) => setSlotDuration(e.target.value)}
+              name="slot-duration-select-options"
+              sx={{ display: "block" }}
+            >
+              <FormControlLabel
+                value="00:15:00"
+                control={<Radio size="small" />}
+                label="15min"
+              />
+              <FormControlLabel
+                value="00:30:00"
+                control={<Radio size="small" />}
+                label="30min"
+              />
+              <FormControlLabel
+                value="01:00:00"
+                control={<Radio size="small" />}
+                label="1h"
+              />
+            </RadioGroup>
+          </FormControl>
         <div className='calendar-list-container'>
           <div className="draggables" ref={draggableRef}>
             <DraggableEvents events={events} />
