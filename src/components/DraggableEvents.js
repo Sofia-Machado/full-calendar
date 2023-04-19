134915ad75378d54fa6eb666df2cc2961f7ed232
Sofia Machado <sofia.machado@riva-office.pt>
2023-04-19 18:00:51 +0100

    pagination not working

--- src/components/DraggableEvents.js
+++ src/components/DraggableEvents.js
@@ -3,21 +3,29 @@ import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
 import dayjs from 'dayjs';
 import { useAddDragItem } from '../hooks/eventHook';
-import { Card, CardContent, Tooltip, Typography } from '@mui/material';
+import { Card, CardContent, IconButton, Tooltip, Typography } from '@mui/material';
 import Zoom from '@mui/material/Zoom';
+import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
+import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
+import { previousDay } from 'date-fns';
 
 const DraggableEvents = ({addNewEvent, events, calendar, removeDraggableEvents, updateExistingEvent}) => {
+  const [page, setPage] = useState(0)
   const [selectedItemId, setSelectedItemId] = useState(null);
   const now = dayjs().format();
   
   const fecthDraggableItems = () => {
-    return axios.get("http://localhost:8080/dragItemList")
+    return axios.get(`http://localhost:8080/dragItemList?_limit=7_page=${page}`)
   }
   const { mutate: addDragItem } = useAddDragItem();
   const queryClient = useQueryClient();
 
+//https://tanstack.com/query/v4/docs/react/guides/paginated-queries
   /* fetch */
-  const { isLoading, data: draggableList, isError, error } = useQuery('dragItems', fecthDraggableItems)
+  const { isLoading, isFetching, data: draggableList, isPreviousData, isError, error } = useQuery(
+    ['dragItems', page], 
+  () => fecthDraggableItems(page),{
+  keepPreviousData : true})
 
   useEffect(() => {
     if (draggableList) {
@@ -81,30 +89,57 @@ const DraggableEvents = ({addNewEvent, events, calendar, removeDraggableEvents, 
   if (isError) {
     return <h2>{error.message}</h2>
   }
-
+console.log(isPreviousData, ' ', draggableList)
   return (
-    <ul className="draggable-list">
-      <Typography variant='title' component='h2' mb={2} sx={{textAlign: 'center', fontSize: 18, fontWeight: 400}}>Évènements à venir</Typography>
-      {draggableList?.data.map((item, index) => {
-        const isSelected = selectedItemId === item.id;
-        return (
-          <Tooltip sx={{m: 0}} title={item?.classNames?.includes('past') ? "Reporter" : ''} TransitionComponent={Zoom} arrow>
-            <Card className="draggable-card" sx={{borderRadius: 20}} key={index} >
-              <CardContent
-              data-event={JSON.stringify(item)}
-              className={`draggable-item ${isSelected ? 'selected' : ''}`}
-              draggable={true}
-              onClick={(e) => handleItemClick(e, item)}
-              >
-                <Typography fontSize={14} sx={{fontWeight: 500}} component="div">
-                  {item.title}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Tooltip>
-        );
-      })}
-    </ul>
+    <>
+      <ul className="draggable-list">
+        <Typography variant='title' component='h2' mb={2} sx={{textAlign: 'center', fontSize: 18, fontWeight: 400}}>Évènements à venir</Typography>
+        {draggableList?.data.map((item, index) => {
+          const isSelected = selectedItemId === item.id;
+          return (
+            <Tooltip sx={{m: 0}} title={item?.classNames?.includes('past') ? "Reporter" : ''}
+            TransitionComponent={Zoom} placement='left' arrow key={index}>
+              <Card className="draggable-card" sx={{borderRadius: 20}} >
+                <CardContent
+                data-event={JSON.stringify(item)}
+                className={`draggable-item ${isSelected ? 'selected' : ''}`}
+                draggable={true}
+                onClick={(e) => handleItemClick(e, item)}
+                >
+                  <Typography fontSize={14} sx={{fontWeight: 500}} component="div">
+                    {item.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Tooltip>
+          );
+        })}
+        <div className='pagination-buttons'>
+          <IconButton onClick={() =>
+          {
+            console.log('click')
+            setPage(old => Math.max(old - 1, 0))}}
+            disabled={page === 0}>
+            <input hidden accept="image/*" type="file" />
+            <KeyboardArrowLeftRoundedIcon />
+          </IconButton>
+              <span>{page + 1}</span>
+          <IconButton
+          onClick={() => {
+            console.log('click')
+            if (!isPreviousData && draggableList.data.length > 7) {
+              setPage(prev => prev + 1)
+            }
+          }}
+          disabled={isPreviousData || !draggableList.data.length > 7}
+          >
+            <input hidden accept="image/*" type="file" />
+            <KeyboardArrowRightRoundedIcon />
+          </IconButton>
+          {isFetching ? <span> Loading...</span> : null}{' '}
+        </div>
+      </ul>
+    </>
   );
 };
 
